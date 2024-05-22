7abaec0f21145637e43cd981e790aad12114a3e8
shivam <shiva2641998@gmail.com>
2024-05-23 02:39:53 +0530

    set all 8

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
 import babel from 'rollup-plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
+import replace from '@rollup/plugin-replace';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
 
 export default [
   {
@@ -11,16 +15,33 @@ export default [
       {
         file: 'dist/index.js',
         format: 'cjs',
+        external: ['React']
       },
     ],
     plugins: [
+      peerDepsExternal(),
+    resolve({
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    }),
+    commonjs({
+      include: 'node_modules/**',
+      namedExports: {
+        'react': ['createElement', 'Component', 'useState', 'useEffect'], // Add other named exports you need
+        'react-dom': ['render']
+      }
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-react']
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+      preventAssignment: true
+    }),
+    terser(),
       postcss({
         plugins: [],
         minimize: true,
-      }),
-      babel({
-        exclude: 'node_modules/**',
-        presets: ['@babel/preset-react']
       }),
       external(),
       resolve(),
