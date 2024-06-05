5426edc99b37aa5d46d0ae1ec77c0403b18040fe
shivam <shiva2641998@gmail.com>
2024-06-06 02:05:49 +0530

    fix all again 14

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import Chat from './components/Chat';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import Chat from "./components/Chat";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Chat title='Welcome'/>
+      <Chat title="Welcome" />
     </Provider>
   </React.StrictMode>
 );
+
+export const ChatBox = ({ ...rest }) => {
+  return (
+        <Provider store={store}>
+          <Chat {...rest} />
+        </Provider>
+  );
+};
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main.js', // Entry point of your application
+  entry: './src/index.js', // Entry point of your application
   output: {
     path: path.resolve(__dirname, 'dist'), // Output directory
     filename: 'index.js', // Output file name
