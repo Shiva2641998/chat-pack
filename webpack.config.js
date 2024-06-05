bbd0da62e2c03b0214ffdfc87b7993a4d5e2c048
shivam <shiva2641998@gmail.com>
2024-06-06 01:46:45 +0530

    fix all again 12

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js', // Entry point of your application
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'index.js', // Output file name
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // Matches both .js and .jsx files
+        exclude: /node_modules/, // Excludes node_modules directory
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/, // Matches .css files
+        use: ['style-loader', 'css-loader'], // Use both style-loader and css-loader
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Automatically resolve these extensions
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
