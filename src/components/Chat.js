1d9c3df751fe073bf29c0687ae4e223f8a1c2ae1
shivam <shiva2641998@gmail.com>
2024-06-06 00:40:15 +0530

    fix all again 1

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import './Chat.css';
-import packageJson from '../../package.json'; 
 import { Counter } from '../features/counter/Counter';
 
 function Chat({title = ""}) {
 
-const version = packageJson.version;
 
   return (
     <div className="App">
       <Counter />
-      <h1>{title} Shivam Kumar {version}</h1>
+      <h1>{title} Shivam Kumar 1.13 </h1>
     </div>
   );
 }
