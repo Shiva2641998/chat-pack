b90581ffac4ad0bf2b182a0d185c812d0731c470
shivam <shiva2641998@gmail.com>
2024-06-06 01:09:53 +0530

    fix all again 7

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import './Chat.css';
-import { Counter } from '../features/counter/Counter';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, selectCount } from '../features/counter/counterSlice';
+// import { Counter } from '../features/counter/Counter';
 
 function Chat({title = ""}) {
-
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
-      <Counter />
-      <h1>{title} Shivam Kumar 1.13 </h1>
+      {/* <Counter /> */}
+      {count}
+      <h1 onClick={() => dispatch(increment())}>{title} Shivam Kumar 1.13 </h1>
     </div>
   );
 }
