c8274cc17f7ecf7235b16bb774e7a247788621b1
shivam <shiva2641998@gmail.com>
2024-06-06 00:44:06 +0530

    fix all again 2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { Provider } from "react-redux";
 import Chat from "./components/Chat";
 import { store } from "./app/store";
+import React from 'react'
 
 const ChatBox = ({...rest}) => {
     return <Provider store={store}>
