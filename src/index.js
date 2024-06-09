7ac8a4b73b124d50dca0d211b4763e8942e598fa
shivam <shiva2641998@gmail.com>
2024-06-09 18:17:55 +0530

    fix all again 25

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
+import * as ReactDOM from "react-dom/client";
+import * as React from "react";
 import Chat from "./components/Chat";
 
-const ChatBox = ({ title }) => {
-  return (
-      <>
-        <Chat title={title} />
-      </>
-  );
-};
+const container = document.querySelector("saytv-chat");
+const shadowContainer = container.attachShadow({ mode: "open" });
+const shadowRootElement = document.createElement("div");
+shadowContainer.appendChild(shadowRootElement);
 
-export { ChatBox };
+
+ReactDOM.createRoot(shadowRootElement).render(
+  <React.StrictMode>
+        <p>Shadow DOM</p>
+        <Chat title="Welcome" />
+  </React.StrictMode>
+);
+
