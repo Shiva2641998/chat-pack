0e6a01189841abf898e6ae28cc845e30b6692136
shivam <shiva2641998@gmail.com>
2024-06-09 14:47:10 +0530

    fix all again 22

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-import React, {useMemo} from "react";
-import { Provider } from "react-redux";
 import Chat from "./components/Chat";
-import { store } from "./app/store";
 
 const ChatBox = ({ title }) => {
   return (
-      <Provider store={store}>
+      <>
         <Chat title={title} />
-      </Provider>
+      </>
   );
 };
 
